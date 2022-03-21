0464e68fb16097fb1bb983da0b73f989fed81597
= <dco3@cin.ufpe.br>
2022-03-21 14:13:04 -0300

    feat: progressbar ok

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,49 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS, PROGRESS_BAR_SIZES } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      size={size}
+    >
+      <BarWrapper>
+        <InnerBar valueNow={value}>
+          <VisuallyHidden>{value}%</VisuallyHidden>
+        </InnerBar>
+      </BarWrapper>
+    </Wrapper>
+  );
 };
 
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+  height:100%;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: ${(p) => PROGRESS_BAR_SIZES[p.size] + "px"};
+  padding: ${(p) => (PROGRESS_BAR_SIZES[p.size] > 12 ? "4px" : "0")};
+  box-sizing: border-box;
+
+  border-radius: 8px;
+  background: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 8px;
+`;
+
+const InnerBar = styled.div`
+  width: ${(p) => p.valueNow + "%"};
+  height: 100%;
+  background: ${COLORS.primary};
+`;
 export default ProgressBar;
--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,16 @@
 export const COLORS = {
-  primary: 'hsl(240deg 80% 60%)',
-  gray50: 'hsl(0deg 0% 95%)',
-  gray300: 'hsl(0deg 0% 75%)',
-  gray500: 'hsl(0deg 0% 50%)',
-  gray700: 'hsl(0deg 0% 40%)',
-  black: 'hsl(0deg 0% 0%)',
-  transparentGray15: 'hsl(0deg 0% 50% / 0.15)',
-  transparentGray35: 'hsl(0deg 0% 50% / 0.35)',
+  primary: "hsl(240deg 80% 60%)",
+  gray50: "hsl(0deg 0% 95%)",
+  gray300: "hsl(0deg 0% 75%)",
+  gray500: "hsl(0deg 0% 50%)",
+  gray700: "hsl(0deg 0% 40%)",
+  black: "hsl(0deg 0% 0%)",
+  transparentGray15: "hsl(0deg 0% 50% / 0.15)",
+  transparentGray35: "hsl(0deg 0% 50% / 0.35)",
+};
+
+export const PROGRESS_BAR_SIZES = {
+  large: 24,
+  medium: 12,
+  small: 8,
 };
